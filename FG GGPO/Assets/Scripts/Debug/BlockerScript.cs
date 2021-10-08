c2cbe754201efb8c1b41bcdcaef1110884b9e361
Whale <mitr1659@edu.kadk.dk>
2021-10-09 01:11:58 +0200

    Unpushable blocker

---
+++ FG GGPO/Assets/Scripts/Debug/BlockerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockerScript : MonoBehaviour
+{
+    public Collider playerCollider;
+    public Collider playerBlockerCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.IgnoreCollision(playerCollider, playerBlockerCollider, true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
